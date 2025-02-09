45e2b69f32b9f1d92cf7ebbeba461494c4f7b1bb
Md. Injamam Hossain <injamam075@gmail.com>
2025-02-10 01:25:53 +0600

    js func

---
+++ arrowfunc.js
@@ -0,0 +1,22 @@
+// Basic Arrow Function (No Parameters):
+
+// const greet = () => {
+//     console.log("Hello, World!");
+// };
+
+// greet(); // Output: Hello, World!
+
+
+
+// Arrow Function with One Parameter:
+
+// const square = num => num * num;
+
+// console.log(square(5)); // Output: 25
+
+
+// Arrow Function with Multiple Parameters:
+
+const add = (a, b) => a + b;
+
+console.log(add(3, 7)); // Output: 10
---
+++ function.js
@@ -0,0 +1,44 @@
+// function sumNumber(num1, num2) {
+//     return num1+num2;
+
+// }
+// console.log(sumNumber(20,40));
+
+
+// function greet() {
+//     console.log("Hello, World!");
+// }
+
+// greet(); // Output: Hello, World!
+
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// let result = add(5, 3);
+// console.log(result); // Output: 8
+
+
+// function greet(name = "Guest") {
+//     console.log("Hello, " + name);
+// }
+
+// greet(); // Output: Hello, Guest
+// greet("Imu"); // Output: Hello, Imu
+
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+// let product = multiply(4, 6);
+// console.log(product); // Output: 24
+
+
+let subtract = function(a, b) {
+    return a - b;
+};
+
+let difference = subtract(10, 4);
+console.log(difference); // Output: 6
